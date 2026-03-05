99d33db043dc6cd35c14fa48b73d051924a7cc0c
KusumGhatawal17 <kusumsaini200417@gmail.com>
2026-03-05 16:18:21 +0530

    removements

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 let h1 = document.getElementById("count");
-let a = 1;
-
+let a = 0;
 
 let increase = document.getElementById("increase");
 increase.addEventListener("click", () => {
-  console.log("click");
+  console.log("increase");
   h1.textContent = ++a;
+  if (a > 0) {
+    h1.style.color = "green";
+  }
+
+  if (a == 0) {
+    h1.style.color = "black";
+  }
 });
 
 let decrease = document.getElementById("decrease");
 decrease.addEventListener("click", () => {
-  console.log("click");
+  console.log("decrease");
   h1.textContent = --a;
+  if (a < 0) {
+    h1.style.color = "red";
+  }
+  if (a == 0) {
+    h1.style.color = "black";
+  }
 });
 
 let reset = document.getElementById("reset");
+
 reset.addEventListener("click", () => {
-  console.log("click");
-  h1.textContent = 0 ;
+  console.log("reset");
+  a = 0;
+  h1.textContent = a;
+  h1.style.color = "black";
 });
